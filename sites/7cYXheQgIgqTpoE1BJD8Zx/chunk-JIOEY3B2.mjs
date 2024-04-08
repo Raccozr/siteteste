8889ffb49f66a2749ffab328e8316e583b855bf1
Raccozr <theux.profissional@gmail.com>
2024-04-08 14:54:55 -0300

    fe
    fefe

---
+++ sites/7cYXheQgIgqTpoE1BJD8Zx/chunk-JIOEY3B2.mjs
@@ -0,0 +1,2 @@
+function a(t,e){return{description:"Made with Framer",title:"My Framer Site"}}export{a};
+//# sourceMappingURL=chunk-JIOEY3B2.mjs.map
---
+++ sites/7cYXheQgIgqTpoE1BJD8Zx/chunk-NBVVGV3A.mjs
@@ -0,0 +1,2 @@
+import{a as t}from"./chunk-JIOEY3B2.mjs";function r(e,a){return{bodyClassName:"framer-body-augiA20Il",breakpoints:[{hash:"72rtr7"}],description:t(e,a).description,elements:{},title:t(e,a).title||"Home",viewport:"width=1200"}}var d=1,m={exports:{metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},default:{type:"function",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{r as a,d as b,m as c};
+//# sourceMappingURL=chunk-NBVVGV3A.mjs.map
